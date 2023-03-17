638e6d038adc9413e0739f3f996f7d55ddc4bd7f
ruxiciortea <ciortearuxandra10@gmail.com>
2023-03-17 16:18:19 +0200

    Created project

---
+++ src/main/java/com/ruxiciortea/Smart/Recipes/SmartRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.ruxiciortea.Smart.Recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRecipesApplication.class, args);
+	}
+
+}
